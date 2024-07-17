9b3528fee7d265449bff54e70575166fe568683e
Jonathan Dev <jonathan.aldana170500@gmail.com>
2024-07-17 00:37:27 -0600

    feat: Dockerizacion de apigateway y generación de dockercompose

--- back/apigateway/routes/index.js
+++ back/apigateway/routes/index.js
@@ -2,35 +2,34 @@ const express = require("express")
 const axios = require('axios');
 const { services } = require('./registry.json');
 const router = express.Router()
+const config = require("../config")
 
+/**
+ * Filter and redirect the routes to the microservices
+ */
 router.all('*', (req, res) => {
-    console.log("Original url: " + req.originalUrl);
     const [ , , , service, ...pathParts] = req.originalUrl.split('/');
     const path = pathParts.join('/');
     const serviceConfig = services[service];
 
-    console.log("Path to request: " + pathParts);
-    console.log("Service to request: " + service);
-    console.log("Url to send: "+ `${serviceConfig.url}${path}`);
-    console.log("Method: " + req.method);
-    console.log("Headers: " + JSON.stringify(req.headers));
-    console.log("Body: " + JSON.stringify(req.body));
     if (!serviceConfig) {
-        console.log("Service: "+ serviceConfig);
         return res.status(404).send({ message: 'Service not found' });
     } 
 
+    console.log("Host que se va a setear: " + config.host);
+
     axios({
+        baseUrl: config.host,
         method: req.method,
         url: `${serviceConfig.url}${path}`,
+        //Validate how to pass headers
         data: req.body
     })
     .then(response => {
-        console.log("Respondio");
         res.send(response.data);
     })
     .catch(error => {
-        console.log("Error de algo: " + error);
+        console.log("Error: " + error);
         if (error.response) {
         res.status(error.response.status).send(error.response.data);
         } else if (error.request) {
@@ -40,7 +39,5 @@ router.all('*', (req, res) => {
         }
     });
 });
-
-
 
 module.exports = router
