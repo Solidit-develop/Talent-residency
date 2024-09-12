9fcec1b41d51a711110159b82ce707ad456f8aa8
Solidit-develop <soliditconsultoria@gmail.com>
2024-09-12 17:06:34 -0600

    Recuperar
    Recuperar mensages para mostrarlos entre usuarios

--- back/services/message-service/src/controllers/menssage.ts
+++ back/services/message-service/src/controllers/menssage.ts
@@ -51,40 +51,67 @@ const controllermessages={
             .getOne();
 
             const usuario = await repositoryuser.findOne({where:{id_user:user_log}})
+
+           
+
             if(!usuario){
                 res.status(404).json({message:"No se encontro el usuario"})
                 return;
             }
 
+            // const fecha = await repositorycoversation.findOneBy({
+            //     where :{id_userOrigen:id_logued},
+            //     relations:["id_userOrigen"]
+            // })
+
             if (!existe){
+
                 console.log("No existe creando la relacion")
                 let conversation = new Conversation();
                 conversation.id_userOrigen=usuario;
                 conversation.id_userDestino= user_des;
-                conversation.creationDate=date;
-                await repositorycoversation.save(conversation)
 
+                conversation.creationDate=date;
+                
+                console.log(conversation)
+
+                // await repositorycoversation.save(conversation)
+         
+
+                console.log(existe)
+
+            
+        
+
+                
+
+
+
+                
+
+                
                 let mensaje = new Messages();
                 mensaje.contect= contect;
                 mensaje.senddate= sendDate
                 mensaje.conversation=conversation;
-                 await repsitorymesages.save(mensaje)
+                //  await repsitorymesages.save(mensaje)
                 console.log("Se regisdtro con exito el mensaje")
+                console.log(mensaje)
 
             }else{
                 let mensaje = new Messages();
                 mensaje.contect= contect;
                 mensaje.senddate= sendDate
-                mensaje.conversation=existe;
-                 await repsitorymesages.save(mensaje)
+                // mensaje.conversation=existe;
+                //  await repsitorymesages.save(mensaje)
+                console.log("Mensaje")
+                console.log(mensaje);
                 console.log("Se regisdtro con exito el mensaje")
             }
 
-           
             
-            
-            
-            res.status(200).json({message:"mensaje guardado con exito"})
+            // res.status(200).json({message:"mensaje guardado con exito"})
+            res.status(200).json({existe})
         }catch(error){
 
             console.log("Hay un error en el servidor ")
@@ -92,10 +119,51 @@ const controllermessages={
             res.status(500).json({Message:"Hay un error interno en el servidor"})
 
         }
+        
+    },
 
-     
+    recupMessages:async(req:Request, res:Response):Promise<void>=>{
         
-        
+        try{
+            let {id_logued,id_dest}= req.params
+
+             
+            const existe = await repositorycoversation.createQueryBuilder("conversation")
+            .leftJoinAndSelect("conversation.id_userOrigen", "userOrigen")  // Relación con el usuario que inició la conversación
+            .leftJoinAndSelect("conversation.messages", "messages")  // Relación con los mensajes
+            .where("(conversation.id_userOrigen = :id_logued AND conversation.id_userDestino = :id_destino)", { id_logued, id_destino: id_dest })  // Condición 1
+            .orWhere("(conversation.id_userOrigen = :id_destino AND conversation.id_userDestino = :id_logued)", { id_logued, id_destino: id_dest })  // Condición 2
+            .getMany();
+         
+            console.log("Mensaje que se nos regres ")
+            console.log(existe);
+
+            
+            // const resultadoenJson = JSON.stringify(existe)
+            // console.log(resultadoenJson)
+
+            console.log(existe)
+
+            const resultados = existe.map(Conversation=>{
+                return{
+                    id_conversation:Conversation.id_conversation,
+                    interactuan:{
+                        nombre:Conversation.id_userOrigen.name_User,
+                        id_dest:Conversation.id_userDestino
+                    },
+                    message:Conversation.messages
+                                     
+                }
+            })
+
+            res.status(200).json({resultados})
+
+        }
+        catch(error){
+            console.log(error)
+            res.status(500).json({message:"Hay un error dentro del servidor"})
+        }
+       
     }
 
 
--- back/services/message-service/src/routes/messages.routes.ts
+++ back/services/message-service/src/routes/messages.routes.ts
@@ -6,5 +6,6 @@ const router= Router();
 router.get('/', controllermessages.ping);
 router.get('/usuarios',controllermessages.usuarios);
 router.get('/prueba/:id_logued/:id_dest',controllermessages.conversacion)
+router.get('/mensajes/:id_logued/:id_dest',controllermessages.recupMessages)
 
 module.exports = router;
