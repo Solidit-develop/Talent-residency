6ecfdd534c2613955a8492f72873ba1ac8de9687
Jonathan Dev <jonathan.aldana170500@gmail.com>
2024-09-12 23:21:04 -0600

    fix: Update routes on users

--- back/services/user-service/src/routes/user.routes.ts
+++ back/services/user-service/src/routes/user.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import usercontroller  from '../controllers/consulta.controller'
+import usercontroller from '../controllers/consulta.controller'
 // const usercontroller = require("../controllers/consulta.controller")
 
-const router= Router();
+const router = Router();
 
 router.get('/ping', usercontroller.ping);
-router.get('/prueba',usercontroller.prueba);
-router.post('/login',usercontroller.inicio_sesion);
-router.post('/insertar',usercontroller.insertusuario);
-router.put('/actualizar/:id',usercontroller.actualizarDatos);
-router.get('/todos',usercontroller.prueba);
-router.post('/envioToken',usercontroller.registroUsuario)
-router.get('/verificacion/:token',usercontroller.verificacion)
-module.exports= router;
+router.get('/prueba', usercontroller.prueba);
+router.post('/login', usercontroller.inicio_sesion);
+router.post('/insertar', usercontroller.insertusuario);
+router.put('/information/:id', usercontroller.actualizarDatos);
+router.get('/todos', usercontroller.prueba);
+router.post('/register', usercontroller.registroUsuario)
+router.get('/validate/:token', usercontroller.verificacion)
+module.exports = router;
