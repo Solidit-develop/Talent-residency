a2f998d4081fa8a962f8391ddfafbc288e367097
Jonathan Dev <jonathan.aldana170500@gmail.com>
2024-11-10 16:13:18 -0600

    typo: Fix table name

--- back/services/proveedores-service/src/controllers/provedor.controllers.ts
+++ back/services/proveedores-service/src/controllers/provedor.controllers.ts
@@ -490,7 +490,7 @@ const controllerProvider = {
                 .leftJoinAndSelect("providers.address", "address")
                 .leftJoinAndSelect("address.town", "town")
                 .leftJoin("town.state", "state")
-                .where("Provider.id_provider=:id_provider", { id_provider: id_provider })
+                .where("Providers.id_provider=:id_provider", { id_provider: id_provider })
                 .getOne();
 
             let id_provedores = proveedores?.id_provider;
