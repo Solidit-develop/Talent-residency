7585235c64295d2ac42d302701d116ea2ec9592c
Solidit-develop <soliditconsultoria@gmail.com>
2024-12-11 13:19:25 -0600

    Ajustes en Docker-compose

--- back/services/appointment-service/src/routes/appointment.routes.ts
+++ back/services/appointment-service/src/routes/appointment.routes.ts
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get("/ping",controllerAppointment.prueba);
 router.post("/cita/:id_provider/:id_customer",controllerAppointment.cita);
-router.post("/relacion/:id_provider/:id_customer/:id_appointment",controllerAppointment.cancelar);
+router.post("/Cancelar/:id_provider/:id_customer/:id_appointment",controllerAppointment.cancelar);
 router.get("/consulta/:id_provider",controllerAppointment.consultaProv)
 router.put("/actualizar/:id_appointment",controllerAppointment.actualizar)
 router.get("/porUser/:id_user",controllerAppointment.consultaUser);
